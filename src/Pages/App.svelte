2c82bd73e6e751998a3f26c488acf62481395273
Alan-Ito <alanthanks4yourmail@gmail.com>
2024-11-03 09:29:10 +0900

    Bottom Navigationをつける

--- src/Pages/App.svelte
+++ src/Pages/App.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+  // Importing router
   import type { Route } from "@mateothegreat/svelte5-router";
   import { route, Router, QueryString } from "@mateothegreat/svelte5-router";
+
+  // Importing UI components
+  import { BottomNav, BottomNavItem, Skeleton, ImagePlaceholder } from 'flowbite-svelte';
+  import { HomeSolid, WalletSolid, AdjustmentsVerticalOutline, UserCircleSolid } from 'flowbite-svelte-icons';
+
+  // Importing pages
   import Homepage from './Homepage.svelte';
   import About from './About.svelte';
   import Settings from './Settings.svelte';
@@ -23,10 +30,28 @@
   ];
 </script>
 
-<div class="flex gap-2">
-  <a use:route href="/">Home</a>
-  <a use:route href="/about">About</a>
-  <a use:route href="/settings">Settings</a>
-</div>
+<BottomNav position="absolute" classInner="grid-cols-5">
+  <BottomNavItem btnName="Home">
+    <a use:route href="/">
+      <HomeSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+    </a>
+  </BottomNavItem>
+  <BottomNavItem btnName="About">
+    <a use:route href="/about">
+      <WalletSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+    </a>
+  </BottomNavItem>
+  <BottomNavItem btnName="Settings">
+    <a use:route href="/settings">
+      <AdjustmentsVerticalOutline class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+    </a>
+  </BottomNavItem>
+  <BottomNavItem btnName="Profile">
+    <UserCircleSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+  </BottomNavItem>
+  <BottomNavItem btnName="Profile2">
+    <UserCircleSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+  </BottomNavItem>
+</BottomNav>
 
 <Router base="/" {routes} />
